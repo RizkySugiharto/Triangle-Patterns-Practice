08651ce97b80df066dc81d621437ee9eabc2a0e7
RizkySugiharto <87899571+RizkySugiharto@users.noreply.github.com>
2023-03-21 20:34:10 +0700

    Cleaning the code

--- main.py
+++ main.py
@@ -25,14 +25,14 @@ Patterns:
 ------------------------------
 """
 type_choices = {
-  1: lambda size: "\n".join([f'* {"  "*i}*' for i in range(size-1)]) + f"\n* {'* '*(size-1)}* ",
-  2: lambda size: "\n".join([f'{"  "*(size-i)}{"* "*((i-1)*2+1)}' for i in range(1, size+1)]),
-  3: lambda size: "\n".join(["*  "*i for i in range(1, size+1)] + ["*  "*(size-i) for i in range(1, size)]),
-  4: lambda size: "\n".join(["* "*i for i in range(1, size+1)]),
-  5: lambda size: "\n".join([f"{'  '*i}{'* '*((size-(i+1))*2+1)}" for i in range(size)]),
-  6: lambda size: "\n".join([f"{'  '*(size-i)}{'* '*i}" for i in range(1, size+1)] + [f"{'  '*i}{'* '*(size-i)}" for i in range(1, size)]),
-  7: lambda size: "\n".join([f"{'  '*(size-i)}{'* '*i}" for i in range(1, size+1)]),
-  8: lambda size: "\n".join([f"{'  '*(size-i)}* {'  '*(i-1)}* " for i in range(1, size)]) + f"\n{'* '*(size+1)}"
+  1: lambda size: "\n".join([f'* {"  " * i}*' for i in range(size - 1)]) + f"\n* {'* '*(size - 1)}* ",
+  2: lambda size: "\n".join([f'{"  " * (size - i)}{"* "*((i - 1) * 2 + 1)}' for i in range(1, size + 1)]),
+  3: lambda size: "\n".join(["*  " * i for i in range(1, size + 1)] + ["*  " * (size - i) for i in range(1, size)]),
+  4: lambda size: "\n".join(["* " * i for i in range(1, size + 1)]),
+  5: lambda size: "\n".join([f"{'  ' * i}{'* ' * ((size - (i + 1)) * 2 + 1)}" for i in range(size)]),
+  6: lambda size: "\n".join([f"{'  ' * (size - i)}{'* ' * i}" for i in range(1, size +1 )] + [f"{'  ' *i }{'* ' * (size - i)}" for i in range(1, size)]),
+  7: lambda size: "\n".join([f"{'  ' * (size - i)}{'* ' * i}" for i in range(1, size + 1)]),
+  8: lambda size: "\n".join([f"{'  ' * (size - i)}* {'  ' * (i - 1)}* " for i in range(1, size)]) + f"\n{'* ' * (size + 1)}"
 }
 
 print(start_message)
